fb9c2602354fb08ac9fa74a0a0c7d694b8cb85c0
hrigg <rigg.heather6@gmail.com>
2022-08-19 12:18:17 -0400

    add 5 dessert data

--- dessertsmodels.js
+++ dessertsmodels.js
@@ -6,9 +6,26 @@ const desserts= [
 },
   {name: "Funfetti Cookie", 
   price: 2, 
-  image: 'https://sallysbakingaddiction.com/wp-content/uploads/2017/06/moist-chocolate-cupcakes-5.jpg', 
-  flavors: ["Double Chocolate, Dark Chocolate, White Chocolate"]
+  image: 'http://thewoodandspoon.com/wp-content/uploads/2017/07/Last-Import-1-of-47-16.jpg', 
+  flavors: ["Rainbow", "Fall Colors", "Red, White, Blue"]
   }, 
+  {name: "Gummy Bears", 
+  price: 3, 
+  image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Oursons_g%C3%A9latine_march%C3%A9_Rouffignac.jpg', 
+  flavors: ["Rainbow", "Strawberry", "Peach"]
+  }, 
+  {name: "Bundt Cake", 
+  price: 20, 
+  image: 'https://butternutbakeryblog.com/wp-content/uploads/2019/02/red-velvet-bundt-cake-1.jpg', 
+  flavors: ["Red Velvet", "Vanilla", "CarrotCake"]
+  }, 
+  {name: "Banana Bread", 
+  price: 5, 
+  image: 'https://www.simplyrecipes.com/thmb/n0haOVlgXcM8Sr53p3C-U53YP2U=/4399x3299/smart/filters:no_upscale()/Simply-Recipes-Easy-Banana-Bread-LEAD-2-2-63dd39af009945d58f5bf4c2ae8d6070.jpg', 
+  flavors: ["Original", "With Nuts", "With Frosting"]
+  }, 
+  
+  
   
 ]
 
