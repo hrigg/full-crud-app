0d3e7b9422cafcd02f85ba5f3dc5d0e0f179fe2c
hrigg <rigg.heather6@gmail.com>
2022-08-19 11:59:07 -0400

    installed express, ejs, nodemon

--- dessertsmodels.js
+++ dessertsmodels.js
@@ -1,11 +1,13 @@
 const desserts= [
-  { name: '', 
-  price:0 , 
-  image: '', 
-  flavors: "" ,
+  { name: 'Chocolate Cupcake', 
+  price: 3, 
+  image: 'https://sallysbakingaddiction.com/wp-content/uploads/2017/06/moist-chocolate-cupcakes-5.jpg', 
+  flavors: ["Double Chocolate", "Dark Chocolate", "White Chocolate"]
 },
-  {
-
+  {name: "Funfetti Cookie", 
+  price: 2, 
+  image: 'https://sallysbakingaddiction.com/wp-content/uploads/2017/06/moist-chocolate-cupcakes-5.jpg', 
+  flavors: ["Double Chocolate, Dark Chocolate, White Chocolate"]
   }, 
   
 ]
