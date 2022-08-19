0791cebacab23cc52ea7255bb783db5ec3a8c4b5
hrigg <rigg.heather6@gmail.com>
2022-08-19 15:29:57 -0400

    added new button and route

--- dessertsmodels.js
+++ dessertsmodels.js
@@ -1,7 +1,7 @@
 const desserts= [
   { name: 'Chocolate Cupcake', 
   price: 3, 
-  image: 'https://unsplash.com/photos/zALugo2vI8c', 
+  image: 'https://sallysbakingaddiction.com/wp-content/uploads/2017/06/moist-chocolate-cupcakes-5.jpg', 
   flavors: ["Double Chocolate", "Dark Chocolate", "White Chocolate"]
 },
   {name: "Funfetti Cookie", 
@@ -9,7 +9,6 @@ const desserts= [
   image: 'http://thewoodandspoon.com/wp-content/uploads/2017/07/Last-Import-1-of-47-16.jpg', 
   flavors: ["Rainbow", "Fall Colors", "Red, White, Blue"]
   }, 
- 
   {name: "Gummy Bears", 
   price: 3, 
   image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Oursons_g%C3%A9latine_march%C3%A9_Rouffignac.jpg', 
@@ -25,7 +24,20 @@ const desserts= [
   image: 'https://www.simplyrecipes.com/thmb/n0haOVlgXcM8Sr53p3C-U53YP2U=/4399x3299/smart/filters:no_upscale()/Simply-Recipes-Easy-Banana-Bread-LEAD-2-2-63dd39af009945d58f5bf4c2ae8d6070.jpg', 
   flavors: ["Original", "With Nuts", "With Frosting"]
   }, 
- 
+  
+
+
+  
+  {name: "", 
+  price: 0, 
+  image: '', 
+  flavors: ["", "", ""]
+  }, 
+  {name: "", 
+  price: 0, 
+  image: '', 
+  flavors: ["", "", ""]
+  }, 
   {name: "Pie", 
   price: 5, 
   image: 'https://www.supermarketperimeter.com/ext/resources/2021/08/19/0819---holiday-pies.jpg?t=1629384170&width=1080', 
@@ -44,7 +56,6 @@ const desserts= [
   flavors: ["Vanilla", "Chocolate", "Cookies and cream", "Mint chocolate"]
 
   },
-
 ]
 
 module.exports= desserts
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 4001;
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => {
     console.log(`App is listening on: ${port}`)
@@ -9,18 +11,32 @@ app.listen(port, () => {
 
 const desserts= require ('./dessertsmodels.js');
 
-app.get('/', (req,res)=>{
-  res.redirect('/desserts')
-})
-
 app.get('/desserts', (req,res)=>{
   const context= {desserts: desserts}
   console.log(desserts[0])
   res.render('index.ejs', context)
 })
+app.get ('/new', (req, res)=>{
+  res.render('new.ejs')
+})
+app.post('/', (req, res)=>{
+  const newFood= req.body
+  desserts.push(newFood)
+  res.redirect('/desserts')
+})
+
+
+
+app.get('/', (req,res)=>{
+  res.redirect('/desserts')
+})
+
+
+
+
 
 app.get('/desserts/:id', (req,res)=>{
-  const context= {desserts: desserts}
+  const context= {desserts: desserts[req.params.id]}
   console.log(desserts[req.params.id])
   res.render('show.ejs', context)
 })
